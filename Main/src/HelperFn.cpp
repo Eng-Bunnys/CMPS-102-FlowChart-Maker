13ccf40e0ddd3bba4f8274aa86dde35e9c630447
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-11-13 21:49:16 +0200

    Added IsVariable function and added comments

--- Main/src/HelperFn.cpp
+++ Main/src/HelperFn.cpp
@@ -34,18 +34,36 @@ bool IsValue(string input) {
 	return HasDigit || (IsNegative && HasDigit);
 }
 
+
+/**
+* Written by Youssef Mohamed Hamdy
+* Check if the given input string is a valid identifier.
+* An identifier should start with a letter (a-z or A-Z) or underscore (_),
+* and can contain letters, numbers (0-9), or underscore (_).
+*
+* @param input The input string to be checked.
+* @return True if the input is a valid identifier, false otherwise.
+*/
 bool IsVariable(string input)
 {
-	// checks if the input string is a variable name
-	// return true if it can be a variable name, false otherwise
+	// If the input length is zero, it is not a valid identifier
+	if (input.empty()) return false;
 
-	// Variable names must start with an alphabetic letter (small or capital) or an underscore (_). 
-	// After the first initial letter, variable names can contain letters, numbers and underscores.  
-	// No spaces or special characters, however, are allowed.
+	char FirstCharacter = input[0];
+	if (!((FirstCharacter >= 'a' && FirstCharacter <= 'z') || (FirstCharacter >= 'A' && FirstCharacter <= 'Z') || FirstCharacter == '_'))
+		return false; // If the first character is not a letter or underscore, it is not a valid identifier
 
-	//TODO: complete this function
+	for (unsigned int i = 1; i < input.length(); ++i)
+	{
+		char CurrentCharacter = input[i];
+		if (!((CurrentCharacter >= 'a' && CurrentCharacter <= 'z')
+			|| (CurrentCharacter >= 'A' && CurrentCharacter <= 'Z')
+			|| (CurrentCharacter >= '0' && CurrentCharacter <= '9')
+			|| CurrentCharacter == '_'))
+			return false; // If any of the characters is not a letter, number, or underscore, it is not a valid identifier
+	}
 
-	return false;
+	return true; // If all characters pass the checks, it is a valid identifier
 }
 
 OpType ValueOrVariable(string input)
