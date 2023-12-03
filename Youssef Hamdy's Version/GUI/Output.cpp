e14886d18354bba02816130def937b03015e6b8b
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-12-03 18:21:31 +0200

    Add files via upload

---
+++ Youssef Hamdy's Version/GUI/Output.cpp
@@ -0,0 +1,165 @@
+#include "Output.h"
+
+
+Output::Output()
+{
+	//Initialize user interface parameters
+	UI.width = 1200;
+	UI.height = 620;
+	UI.wx = 15;
+	UI.wy =15;
+
+	UI.AppMode = DESIGN;	//Design Mode is the default mode
+
+	UI.StatusBarHeight = 50;
+	UI.ToolBarHeight = 50;
+	UI.MenuItemWidth = 80;
+	UI.DrawingAreaWidth = 0.75 * UI.width;
+
+	UI.DrawColor = BLUE;
+	UI.HighlightColor = RED;
+	UI.MsgColor = RED;
+
+	UI.ASSGN_WDTH = 150;
+	UI.ASSGN_HI = 50;
+
+	//Create the output window
+	pWind = CreateWind(UI.width, UI.height, UI.wx, UI.wy);
+	//Change the title
+	pWind->ChangeTitle("Programming Techniques Project");
+	
+	pWind->SetPen(RED,3);
+	CreateDesignToolBar();
+	CreateStatusBar();
+	ClearDrawArea();
+	ClearOutputBar();
+}
+
+
+Input* Output::CreateInput()
+{
+	Input* pIn = new Input(pWind);
+	return pIn;
+}
+
+//======================================================================================//
+//								Interface Functions										//
+//======================================================================================//
+
+window* Output::CreateWind(int wd, int h, int x, int y)
+{
+	return new window(wd, h, x, y);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::CreateStatusBar()
+{
+	pWind->DrawLine(0, UI.height-UI.StatusBarHeight, UI.width, UI.height-UI.StatusBarHeight);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+//TODO: Complete this function
+void Output::CreateDesignToolBar() //Draws the Design Menu
+{
+	UI.AppMode = DESIGN;	//Design Mode
+	
+	//fill the tool bar 
+		
+	//You can draw the tool bar icons in any way you want.
+	//Below is one possible way
+	
+	//First prepare List of images for each menu item
+	//To control the order of these images in the menu, 
+	//reoder them in Defs.h ==> enum DrawMenuItem
+	string MenuItemImages[DSN_ITM_CNT];
+	MenuItemImages[ITM_VALUE_ASSIGN] = "images\\Assign.jpg";
+	MenuItemImages[ITM_COND] = "images\\Condition.jpg";
+	MenuItemImages[ITM_EXIT] = "images\\Exit.jpg";
+	//TODO: Prepare images for each menu item and add it to the list
+
+
+	//Draw menu item one image at a time
+	for(int i=0; i<DSN_ITM_CNT; i++)
+		pWind->DrawImage(MenuItemImages[i], i*UI.MenuItemWidth, 0, UI.MenuItemWidth, UI.ToolBarHeight);
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED, 2);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);	
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+//TODO: Complete this function
+void Output::CreateSimulationToolBar() //Draws the Simulation Menu
+{
+	UI.AppMode = SIMULATION;	//Simulation Mode
+	///TODO: add code to create the simulation tool bar
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::ClearStatusBar()
+{
+	//Clear Status bar by drawing a filled white rectangle
+	pWind->SetPen(RED, 2);
+	pWind->SetBrush(WHITE);
+	pWind->DrawRectangle(0, UI.height - UI.StatusBarHeight, UI.width, UI.height);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::ClearDrawArea()
+{
+	pWind->SetPen(RED, 2);
+	pWind->SetBrush(WHITE);
+	pWind->DrawRectangle(0, UI.ToolBarHeight, UI.DrawingAreaWidth, UI.height - UI.StatusBarHeight);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::ClearOutputBar()
+{
+	//Create output bar by drawing a filled rectangle
+	pWind->SetPen(RED, 2);
+	pWind->SetBrush(LIGHTBLUE);
+	pWind->DrawRectangle(UI.DrawingAreaWidth, UI.ToolBarHeight, UI.width, UI.height - UI.StatusBarHeight);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::PrintMessage(string msg)	//Prints a message on status bar
+{
+	ClearStatusBar();	//First clear the status bar
+	
+	pWind->SetPen(UI.MsgColor, 50);
+	pWind->SetFont(20, BOLD , BY_NAME, "Arial");   
+	pWind->DrawString(10, UI.height - (int) (UI.StatusBarHeight/1.5), msg);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::DrawString(const int iX, const int iY, const string Text)
+{
+	pWind->SetPen(BLACK, 2);
+	pWind->DrawString(iX, iY, Text);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//======================================================================================//
+//								Statements Drawing Functions								//
+//======================================================================================//
+
+//Draw assignment statement and write the "Text" on it
+void Output::DrawAssign(Point Left, int width, int height, string Text, bool Selected)
+{
+	if(Selected)	//if stat is selected, it should be highlighted
+		pWind->SetPen(UI.HighlightColor,3);	//use highlighting color
+	else
+		pWind->SetPen(UI.DrawColor,3);	//use normal color
+
+	//Draw the statement block rectangle
+	pWind->DrawRectangle(Left.x, Left.y, Left.x + width, Left.y + height);
+		
+	//Write statement text
+	pWind->SetPen(BLACK, 2);
+	pWind->DrawString(Left.x+width/4, Left.y + height/4, Text);
+}
+
+//TODO: Add similar functions for drawing all other statements.
+//		e.g. DrawCondtionalStat(......), DrawStart(......), DrawEnd(.......), ...etc
+//		Decide the parameters that should be passed to each of them
+	
+//TODO: Add DrawConnector function
+
+//////////////////////////////////////////////////////////////////////////////////////////
+Output::~Output()
+{
+	delete pWind;
+}
---
+++ Youssef Hamdy's Version/GUI/Output.h
@@ -0,0 +1,41 @@
+#ifndef OUPTUT_H
+#define OUPTUT_H
+
+#include "Input.h"
+
+class Output	//The application manager should have a pointer to this class
+{
+private:	
+	window* pWind;	//Pointer to the Graphics Window
+public:
+	Output();	
+
+	window* CreateWind(int, int, int , int);
+	Input* CreateInput(); //Creates a pointer to the Input object	
+
+	void CreateStatusBar(); //Creates status bar
+
+	//TODO: Complete the following 2 functions
+	void CreateDesignToolBar();	//Tool bar of the design mode
+	void CreateSimulationToolBar(); //Tool bar of the simulation mode
+
+	void ClearStatusBar();	//Clears the status bar
+	void ClearDrawArea();	//Clears the drawing area
+	void ClearOutputBar(); //Clears the Output bar
+	
+	void PrintMessage(string msg);	//Prints a message on Status bar
+	void DrawString(const int, const int, const string);  //Writes a string in the specified location
+
+	// -- Statements Drawing Functions
+	void DrawAssign(Point Left, int width, int height, string Text, bool Selected=false);
+
+	//TODO: Add similar functions for drawing all other statements.
+	//		e.g. DrawCondtionalStat(......), DrawStart(......), DrawEnd(.......), ...etc
+	//		Decide the parameters that should be passed to each of them
+	
+	//TODO: Add DrawConnector function
+
+	~Output();
+};
+
+#endif
