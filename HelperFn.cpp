8df634904a99d487ba3b2dfff37348ace86e3e65
ykismail <152629665+ykismail@users.noreply.github.com>
2023-12-04 12:28:16 +0200

    Update HelperFn.cpp bta3 jooooo

--- HelperFn.cpp
+++ HelperFn.cpp
@@ -1,31 +1,66 @@
 #include <iostream>
 #include "Helper.h"
-using namespace std; //7atetha just in case but it worked perfectly fine without it in the visual studio.
+#include <vector>
+#include <algorithm>
+using namespace std;
+void removeSpaces(string& input) {
+	input.erase(remove(input.begin(), input.end(), ' '), input.end());
+}
 bool IsValue(string input) {
 	if (input.length() == 0) {
 		return false;
-	}
+	} removeSpaces(input);
+	bool hasadot = false, hasadigit = false, hasanegative = false;
 	for (int i = 0; i < input.length(); i++) {
-		bool hasadot = false;
 		char currentcharacter = input[i];
-		if (!(currentcharacter == '.' || (currentcharacter >= '0' && currentcharacter <= '9'|| currentcharacter=='-'))) {
+		if (currentcharacter == '-' && i != 0) {
 			return false;
 		}
-
-		if (currentcharacter == '-') {
-			if (i != 0) {
-				return false;
-			}
+		else if (currentcharacter == '-' && i == 0) {
+			hasanegative = true;
 		}
-		if (currentcharacter == '.') {
-			if (i == 0) {
+		else if (currentcharacter == '.' && hasanegative == true && i == 1) {
+			return false;
+		}
+		else if (currentcharacter == '.' && i == 0) {
+			return false;
+		}
+		else if (currentcharacter == '.') {
+			if (hasadot == true) {
 				return false;
 			}
-			else hasadot = true;
-		} 
-	} return true;
+			hasadot = true;
+		}
+		else if (!((currentcharacter >= '0' && currentcharacter <= '9') || currentcharacter == 'e' || currentcharacter == '-' || currentcharacter == ' ')) {
+			return false;
+		}
+		else {
+			hasadigit = true;
+		}
+	} return hasadigit;
+}
+bool isCppKeyword(const string& input) {
+	// List of C++ keywords (you can expand this list as needed)
+	vector<string> keywords = {
+		"auto", "break", "case", "char", "const", "continue", "default", "do", "double",
+		"else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register",
+		"return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef",
+		"union", "unsigned", "void", "volatile", "while"
+		// Add more keywords if needed
+	};
+
+	// Convert the input string to lowercase for case-insensitive comparison
+	string lowercaseInput = input;
+	transform(lowercaseInput.begin(), lowercaseInput.end(), lowercaseInput.begin(), ::tolower);
+
+	// Check if the lowercase input is in the list of keywords
+	return find(keywords.begin(), keywords.end(), lowercaseInput) != keywords.end();
+}
 bool IsVariable(string input) {
 	if (input.length() == 0) {
+		return false;
+	}
+	if (isCppKeyword(input)) {
 		return false;
 	}
 	char first = input[0];
